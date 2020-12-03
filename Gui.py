23a496ab8eaadcd3e10fe3d96838257204ee671d
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2020-12-03 19:08:41 +0000

    Change interface language

--- Gui.py
+++ Gui.py
@@ -8,7 +8,7 @@ def gui():
     gui_variables = []
     # ================================================== Interface grafica =======================================================================
     root = tk.Tk()
-    root.title("Menu inicial")
+    root.title("Initial Menu")
     root.configure(background="#C6C5B9")
 
     # Ler as imagens
@@ -19,7 +19,7 @@ def gui():
     # Botao de informacoes
 
     def openInfo():
-        messagebox.showinfo('Informação', "Controlos: \n\ni - informação relativa à ddp\ne - valor media de energia cinetica nesse instante\nn - adiciona ioes de sodio\nk - adiciona ioes de potassio\na - remove ioes de sodio\ns - remove ioes de potassio\nESC - pausar/continuar\nseta esq/dir - muda a cor do fundo")
+        messagebox.showinfo('Information', "Controls: \n\ni - Information related with Voltage\ne - Instant Kinetic Energy Value\nn - Add sodium ions\nk - add potassium ions\na - remove sodium ions\ns - remove potassium ions\nESC - Pause/Resume\nLeft/Righ arrow key - change backgound color")
 
     # Guarda os valores inseridos numa lista e destroi a janela da interface
 
@@ -30,7 +30,7 @@ def gui():
             number_Na = Na_entry.get()
             number_K = K_entry.get()
             if int(number_K) + int(number_Na) > 250:
-                msg_box = tk.messagebox.askquestion(title="AVISO!", message="Um número de iões superior a 250 pode afetar seriamente o desempenho do programa. Tem a certeza que deseja continuar?", icon='warning')
+                msg_box = tk.messagebox.askquestion(title="Warning!", message="A number above 250 ions may drastically reduce the performance of the simulation. Wish to proceed?", icon='warning')
                 if msg_box == 'yes':
                     gui_variables.append(color_k)
                     gui_variables.append(color_Na)
@@ -46,7 +46,7 @@ def gui():
                 gui_variables.append(number_K)
                 root.destroy()
         except Exception:
-            print("Ocorreu um erro com os valores inseridos")
+            print("Invalid values")
 
     # Permite escolher a cor das particulas de sodio
 
@@ -56,7 +56,7 @@ def gui():
             colorEntryNa.delete(0, tk.END)
             colorEntryNa.insert(tk.END, Na_color[0])
         except Exception:
-            print("Nao foi selecionada nenhuma cor para o sodio. Insira uma cor para continuar")
+            print("No color selected for sodium. Insert a color to proceed")
 
     # Permite escolher a cor das particulas de potassio
 
@@ -66,7 +66,7 @@ def gui():
             colorEntryK.delete(0, tk.END)
             colorEntryK.insert(tk.END, K_color[0])
         except Exception:
-            print("Nao foi selecionada nenhuma cor para o potassio. Insira uma cor para continuar")
+            print("No color selected for potassium. Insert a color to proceed")
 
     # Atribui valores default a todas as entradas de texto
 
@@ -86,10 +86,10 @@ def gui():
     # Butoes, labels e entradas de texto
     isep_log = tk.Label(root, image=isep_icon, padx=10, pady=10, bd=0, background="#C6C5B9")
 
-    Na_label = tk.Label(root, text="Nº de Sódios", pady=5, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
-    K_label = tk.Label(root, text="Nº de Potássios", pady=5, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
-    labelColor1 = tk.Label(root, text="Cor do ião", pady=5, padx=33, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
-    labelColor2 = tk.Label(root, text="Cor do ião", pady=5, padx=33, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
+    Na_label = tk.Label(root, text="Nº of sodiums", pady=5, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
+    K_label = tk.Label(root, text="Nº of potassiums", pady=5, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
+    labelColor1 = tk.Label(root, text="Ion color", pady=5, padx=33, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
+    labelColor2 = tk.Label(root, text="Ion color", pady=5, padx=33, justify=tk.LEFT, bg="#393D3F", fg="#FFFFFF")
 
     Na_entry = tk.Entry(root)
     K_entry = tk.Entry(root)
@@ -98,9 +98,9 @@ def gui():
 
     colorNa_button = tk.Button(root, image=colorpicker_icon, compound=tk.CENTER, command=chooseNacolor, bd=3, bg="#FFFFFF")
     colorK_button = tk.Button(root, image=colorpicker_icon, compound=tk.CENTER, command=chooseKcolor, bd=3, bg="#FFFFFF")
-    default_button = tk.Button(root, text="Inserir valores default", command=defaultValues, padx=4, pady=4, bg="#393D3F", fg="#FFFFFF")
+    default_button = tk.Button(root, text="Insert Default Values", command=defaultValues, padx=4, pady=4, bg="#393D3F", fg="#FFFFFF")
 
-    startButton = tk.Button(root, text="Simular", command=startProgram, padx=4, pady=4, bg="#393D3F", fg="#FFFFFF")
+    startButton = tk.Button(root, text="Start Simulation", command=startProgram, padx=4, pady=4, bg="#393D3F", fg="#FFFFFF")
     infoButton = tk.Button(root, image=info_icon, padx=0, command=openInfo, justify=tk.LEFT, bd=0, bg="#C6C5B9")
 
     # Meter os widgets no ecra
